28d5605667607daa6a66a400760e2849e06d9487
Gziiiiiiiii <50094637+Gziiiiiiiii@users.noreply.github.com>
2019-09-03 22:30:23 -0400

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -16,10 +16,10 @@ data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
 ##Convert Time var to Time class
 data$datetime <- strptime(paste(data$Date,data$Time), "%Y-%m-%d %H:%M:%S")
 
-##Plot graph
+##Plot graph 1
 attach(data)
 hist(Global_active_power,main="Global Active Power",
-     xlab = "Global Active Power (kilowatts",
+     xlab = "Global Active Power (kilowatts)",
      ylab = "Frequency", col = "Red")
 
 dev.copy(png, file = "plot1.png", height = 480, width = 480)
